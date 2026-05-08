ed88def0333428410b3eae22a340c136044f6b7c
Taufiq <1202407005@students.itspku.ac.id>
2026-05-08 09:25:48 +0700

    update

--- register.php
+++ register.php
@@ -1,11 +1,22 @@
+
 <?php
+// mengambil file koneksi database
+
 include 'config/koneksi.php';
+ //isset digunakan untuk mengecek 
+ //apakah tombol register di tekan 
 if (isset($_POST['register'])) {
     
+    // Mengambil data dari input from 
     $username = $_POST['username'];
+
+    //md5 digunakan untuk mengenkripsi password
     $password = md5($_POST['password']);
 
     mysqli_query($conn, "Insert INTO user VALUES('', ' $username','$password')");
+
+    //setelah berhasil register
+    //pindah ke halaman login 
 
     header("Location: login.php");
 }
